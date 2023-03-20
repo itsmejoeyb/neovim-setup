75b571b6e0018c2aeafa08fcf30bc6c6d9f2d264
Joey <31454088+itsmejoeyb@users.noreply.github.com>
2023-03-20 10:11:25 -0400

    hopefully fixed scrolloff

--- init.lua
+++ init.lua
@@ -206,7 +206,7 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Scroll line padding
-vim.wo.scrolloff = 8
+vim.o.scrolloff = 8
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -377,7 +377,7 @@ require('nvim-treesitter.configs').setup {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
---vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- LSP settings.
